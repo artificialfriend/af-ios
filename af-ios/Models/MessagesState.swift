25e647bb45a46773303f02ab1cfae8a44c1522a8
conscioustahoe <ashutoshnarang80@gmail.com>
2023-02-02 01:55:17 +0530

    hook chat api

--- af-ios/Models/MessagesState.swift
+++ af-ios/Models/MessagesState.swift
@@ -33,47 +33,35 @@ class MessagesState: ObservableObject {
     }
     
     struct RequestBody: Codable {
-        let userId: String
+        let user_id: String
         let text: String
-        let isPrompt: Bool
+        let is_prompt: Bool
     }
-
+    
+    struct APIResponse: Decodable {
+        let response: String
+    }
+    
     func generateResponse(text: String) {
+        let requestBody = RequestBody(user_id: "1", text: text, is_prompt: true)
         let url = URL(string: "https://af-backend-gu2hcas3ba-uw.a.run.app/chat/")!
-        let requestBody = RequestBody(userId: "1", text: text, isPrompt: true)
-        makePostRequest(url: url, requestBody: requestBody)
-    }
-
-    func makePostRequest(url: URL, requestBody: RequestBody) {
-        var semaphore = DispatchSemaphore (value: 0)
-
-        let parameters = "{\n    \"user_id\": \"1\",\n    \"text\": \"explain episodic memory like i am 5\",\n    \"is_prompt\": true\n}"
-        let postData = parameters.data(using: .utf8)
-
-        var request = URLRequest(url: URL(string: "https://af-backend-gu2hcas3ba-uw.a.run.app/chat/")!,timeoutInterval: Double.infinity)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.httpBody = postData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try! JSONEncoder().encode(requestBody)
 
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-          guard let data = data else {
-            print(String(describing: error))
-            semaphore.signal()
-            return
-          }
-            let message = String(data: data, encoding: .utf8)!
-            print(String(data: data, encoding: .utf8)!)
-            self.addMessage(text: message, byAF: true, isNew: true)
-            semaphore.signal()
-        }
+        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard let data = data else { return }
 
-        task.resume()
-        semaphore.wait()
+            let apiResponse = try! JSONDecoder().decode(APIResponse.self, from: data)
+            DispatchQueue.main.async {
+                self?.addMessage(text: apiResponse.response, byAF: true, isNew: true)
+            }
+        }.resume()
     }
 
     @Published var messages: [Message] = [
-        Message(text: "Summarize chapter 2 of wuthering heights", byAF: false, isNew: false, timestamp: Date.now),
-        Message(text: "Chapter 2 of Wuthering Heights introduces the character of Mr. Lockwood, a tenant of Thrushcross Grange, who travels to Wuthering Heights to meet his landlord, Heathcliff. During his visit, he is treated with disdain by Heathcliff and the other residents of the house. Mr. Lockwood is also unsettled by the ghostly presence of Catherine, who he hears wandering the halls at night. The chapter sets the stage for the tumultuous relationships and events that will unfold throughout the rest of the novel.", byAF: true, isNew: false, timestamp: Date.now)
+//        Message(text: "Summarize chapter 2 of wuthering heights", byAF: false, isNew: false, timestamp: Date.now),
+//        Message(text: "Chapter 2 of Wuthering Heights introduces the character of Mr. Lockwood, a tenant of Thrushcross Grange, who travels to Wuthering Heights to meet his landlord, Heathcliff. During his visit, he is treated with disdain by Heathcliff and the other residents of the house. Mr. Lockwood is also unsettled by the ghostly presence of Catherine, who he hears wandering the halls at night. The chapter sets the stage for the tumultuous relationships and events that will unfold throughout the rest of the novel.", byAF: true, isNew: false, timestamp: Date.now)
     ]
 }
